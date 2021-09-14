bee1699669f0ec78c3557c3d2bd5d8c2f346ac2c
lornop <90425445+lornop@users.noreply.github.com>
2021-09-13 21:24:47 -0700

    Add project files.

---
+++ ConsoleCalculator/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab02ConsoleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please eneter a number");
+            double num1 = Convert.ToInt32(Console.ReadKey());
+
+            Console.WriteLine("Enter a second number");
+            double num2 = Convert.ToInt32(Console.ReadKey());
+
+
+            Console.WriteLine("What operation would you like to do?");
+            Console.WriteLine("1 = Add, 2 = Subtract");
+            Console.WriteLine("3 = Multiply, 4 = Divide");
+
+            int operation_num = Convert.ToInt32(Console.ReadKey());
+            switch (operation_num)
+            {
+                case 1:
+                    //do a add
+                    break;
+
+                case 2:
+                    //do a subtract
+                    break;
+
+                case 3:
+                    //do a multiply
+                    break;
+
+                case 4:
+                    //do a divide
+                    break;
+
+            }
+
+
+        }
+    }
+}
